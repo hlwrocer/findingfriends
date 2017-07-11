3e1c9dbaeba210a54b96d7a0795cb6d75ae79da6
hlwrocer <duanhanson@gmail.com>
2017-07-11 14:43:31 -0400

    add sort (no trump yet)

--- cards.js
+++ cards.js
@@ -4,7 +4,7 @@ function genWholeRand(hi){
 }
 
 //card object
-//for jokers, value - 15 big 14 small name = SJ/BJ, suit = Joker
+//for jokers, value - 16 big 15 small name = SJ/BJ, suit = Joker, Ace = 14
 function Card(value, name, suit){
 	this.value = value;
 	this.name = name;
@@ -15,19 +15,19 @@ function Card(value, name, suit){
 function Deck(){
 
 	this.cards = (function(){
-		var names = ['1','2','3','4','5','6','7','8','9','10','J','Q','K'];
+		var names = ['2','3','4','5','6','7','8','9','10','J','Q','K', 'A'];
 		var suits = ['H','C','D','S'];
 		var cards = [];
 		//gen 52 suited cards
 		for (var suit = 0; suit < suits.length; suit++){
 			for (var name = 0; name < names.length; name++){
-				cards.push( new Card(name+1,names[name], suits[suit]));
+				cards.push( new Card(name+2,names[name], suits[suit]));
 			}
 		}
 		
 		//two jokers
-		cards.push(new Card (14, 'SJ', 'Joker'));
-		cards.push(new Card (15, 'BJ', 'Joker'));
+		cards.push(new Card (15, 'SJ', 'Joker'));
+		cards.push(new Card (16, 'BJ', 'Joker'));
 		
 		return cards
 	})();
@@ -117,6 +117,45 @@ Player.prototype = {
 		var diamonds = [];
 		var clubs = [];
 		var trump = [];
+		var merge = function(arr1, arr2){
+			var mergeArr = [];
+			while (arr1.length && arr2.length){
+				if arr1[0]. <= arr2[0]{
+					mergeArr.push(arr1[0].shift());	
+				}
+				else{
+					mergeArr.push(arr2[0].shift());
+				}
+			}
+			while(arr1.length){
+				mergeArr.push(arr1[0].shift());	
+			}
+			
+			while(arr2.length){
+			      mergeArr.push(arr2[0].shift());
+			}
+			
+			return mergeArr;
+		};
+		var sortSuit = function(cards, isTrump=false, trumpSuit=null){
+			//probably merge sort for not trump.
+			//possibly unneeded? If cards dealt one at a time sort/insert that card object one at a time, sequentially
+			//^ might need for trump e.g trump suit = clubs flip to hearts. Or allow for manual moving of cards
+			// and add a sort button. probably the latter
+			if (isTrump){
+				//TODO
+				return cards;
+			}
+			else{
+				if (cards.length < 2){
+					return cards
+				}
+				else{
+					var left = cards.splice(0,Math.floor(cards.length/2));
+					return merge(sortSuit(left), sortSuit(cards));
+				}
+			}
+		}
 		//sort into suits
 		for (var card = 0; card < (this.cards).length; card++){
 			if (card.suit === "c" && card.value !== trumpNum){
@@ -125,25 +164,42 @@ Player.prototype = {
 			else if (card.suit === "d" && card.value !== trumpNum){
 				diamonds.push(card);
 			}
-			else if (cards.suit === "s" && card.value !== trumpNum){
+			else if (card.suit === "s" && card.value !== trumpNum){
 				spades.push(card);
 			}
-			else if (cards.suit === "h" && card.value !== trumpNum){
+			else if (card.suit === "h" && card.value !== trumpNum){
 				hearts.push(card);
 			}
 			else{
 				trump.push(card);	
 			}
 		}
-	},
-	sortSuit: function sortSuit(cardArray, isTrump=false, trumpSuit=null){
-		//probably merge sort
-		//possibly unneeded? If cards dealt one at a time sort/insert that card object one at a time, sequentially
-		//^ might need for trump e.g trump suit = clubs flip to hearts. Or allow for manual moving of cards
-		// and add a sort button.
-		//possibly just define within sortHand to scope as a helper method?
-		return cardArray
+		clubs = sortSuit(clubs);
+		hearts = sortSuit(hearts);
+		diamonds = sortSuit(diamonds);
+		spades = sortSuit(spades);
+		trump = sortSuit(trump);
+		
+		if (trumpSuit === 'h'){
+			this.cards = [].concat(clubs,diamonds,spades,hearts,trump);
+		}
+		else if (trumpSuit === 'c'){
+			this.cards = [].concat(hearts,spades,diamonds,clubs,trump);
+		}
+		else if (trumpSuit === 'd'){
+			this.cards = [].concat(spades,hearts,clubs,diamonds,trump);
+		}
+		else{
+			this.cards = [].concat(diamonds,clubs,hearts,spades,trump);
+		}
+		
 	}
 }
 
-	
+function evalCards(cards){
+	//input: array of card objects
+	//evaluates what was played, e.g is it a pair idk how to describe this
+	//to be placed somewhere, not exactly sure yet, need to do ui and figure this out.
+	//played cards must be playable, have player's playCard function handle this. E.g you cannot throw ace and 4 of hearts if you have another heart somewhere, however you can try and throw all of them (shuai)
+	//idk figure this out later not entirely sure how to structure yet without server client code
+}
